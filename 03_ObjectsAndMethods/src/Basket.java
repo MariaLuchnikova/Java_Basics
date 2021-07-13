9c9bd078041ae8927cf5e879df533de13ba8d5a7
mariia_luchnikova <luchniklav@yandex.ru>
2021-07-13 18:49:26 +0800

    add totalWeight

--- 03_ObjectsAndMethods/src/Basket.java
+++ 03_ObjectsAndMethods/src/Basket.java
@@ -4,6 +4,7 @@ public class Basket {
     private String items = "";
     private int totalPrice = 0;
     private int limit;
+    private double totalWeight = 0;
 
     public Basket() {
         increaseCount(1);
@@ -16,10 +17,11 @@ public class Basket {
         this.limit = limit;
     }
 
-    public Basket(String items, int totalPrice) {
+    public Basket(String items, int totalPrice, double totalWeight) {
         this();
         this.items = this.items + items;
         this.totalPrice = totalPrice;
+        this.totalWeight = totalWeight;
     }
 
     public static int getCount() {
@@ -30,9 +32,7 @@ public class Basket {
         Basket.count = Basket.count + count;
     }
 
-    public void add(String name, int price) {
-        add(name, price, 1);
-    }
+    public void add(String name, int price) { add(name, price, 1); }
 
     public void add(String name, int price, int count) {
         boolean error = false;
@@ -49,19 +49,29 @@ public class Basket {
             return;
         }
 
-        items = items + "\n" + name + " - " +
-            count + " шт. - " + price;
         totalPrice = totalPrice + count * price;
+
+        items = items + "\n" + name + " - " +
+                count + " шт. - " + price + "\n" + "Общая стоимость товаров: " + totalPrice;
+    }
+
+    public void add(String name, int price, int count, double weight)
+    {
+      totalWeight = totalWeight + weight * count;
+      items = "\n" + "Масса всех товаров корзине: " + totalWeight;
     }
 
     public void clear() {
         items = "";
         totalPrice = 0;
+        totalWeight = 0;
     }
 
     public int getTotalPrice() {
         return totalPrice;
     }
+
+    public double getTotalWeght() { return  totalWeight; }
 
     public boolean contains(String name) {
         return items.contains(name);
--- 03_ObjectsAndMethods/src/Main.java
+++ 03_ObjectsAndMethods/src/Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         Basket basket = new Basket();
-        basket.add("Milk", 40);
-        basket.print("Milk");
+        //basket.add("Milk", 40, 2);
+        basket.add("Milk", 40, 2, 500);
+        basket.add("Apple", 50, 3, 300);
+        basket.print("Содержимое корзины: ");
     }
 }
