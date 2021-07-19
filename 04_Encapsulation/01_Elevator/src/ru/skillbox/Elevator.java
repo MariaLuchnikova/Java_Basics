97015a63dc79f545efc40c91f2d8ad54a14ce3a1
mariia_luchnikova <luchniklav@yandex.ru>
2021-07-19 08:47:56 +0800

    modified Elevator

--- 04_Encapsulation/01_Elevator/src/ru/skillbox/Elevator.java
+++ 04_Encapsulation/01_Elevator/src/ru/skillbox/Elevator.java
@@ -3,8 +3,8 @@ package ru.skillbox;
 // при условии, что этажи < 0 - это цокольные
 public class Elevator {
     private int currentFloor = 1;
-    private int minFloor;
-    private int maxFloor;
+    private final int minFloor;
+    private final int maxFloor;
 
     public Elevator(int minFloor, int maxFloor) {
         this.minFloor = minFloor;
@@ -29,15 +29,16 @@ public class Elevator {
         else if (floor > currentFloor) {
             while (currentFloor < floor) {
                 moveUp();
-                System.out.println(currentFloor == 0 ? "0" : currentFloor + " этаж");
+                System.out.println(currentFloor == 0 ? "-" : currentFloor + " этаж");
             }
+            currentFloor = floor;
         } else if (floor < currentFloor) {
             while (currentFloor > floor) {
                 moveDown();
-                System.out.println(currentFloor == 0 ? "0" : currentFloor + " этаж");
+                System.out.println(currentFloor == 0 ? "-" : currentFloor + " этаж");
             }
+            currentFloor = floor;
         }
-        currentFloor = floor;
     }
 }
 
