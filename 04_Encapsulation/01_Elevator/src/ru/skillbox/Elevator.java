1fa91c6584640e94b7eaaaefe6db0b1159d3a2ca
mariia_luchnikova <luchniklav@yandex.ru>
2021-07-19 11:59:00 +0800

    optimisation Elevator

--- 04_Encapsulation/01_Elevator/src/ru/skillbox/Elevator.java
+++ 04_Encapsulation/01_Elevator/src/ru/skillbox/Elevator.java
@@ -2,9 +2,9 @@ package ru.skillbox;
 
 // при условии, что этажи < 0 - это цокольные
 public class Elevator {
-    private int currentFloor = 1;
     private final int minFloor;
     private final int maxFloor;
+    private int currentFloor = 1;
 
     public Elevator(int minFloor, int maxFloor) {
         this.minFloor = minFloor;
@@ -26,17 +26,17 @@ public class Elevator {
     public void move(int floor) {
         if (floor < minFloor || floor > maxFloor || floor == 0)
             System.out.println("Ошибка! " + floor + " этаж отсутствует.");
-        else if (floor > currentFloor) {
-            while (currentFloor < floor) {
-                moveUp();
-                System.out.println(currentFloor == 0 ? "-" : currentFloor + " этаж");
-            }
-            currentFloor = floor;
-        } else if (floor < currentFloor) {
-            while (currentFloor > floor) {
-                moveDown();
-                System.out.println(currentFloor == 0 ? "-" : currentFloor + " этаж");
-            }
+        else {
+            if (floor > currentFloor)
+                while (currentFloor < floor) {
+                    moveUp();
+                    System.out.println(currentFloor == 0 ? "-" : currentFloor + " этаж");
+                }
+            if (floor < currentFloor)
+                while (currentFloor > floor) {
+                    moveDown();
+                    System.out.println(currentFloor == 0 ? "-" : currentFloor + " этаж");
+                }
             currentFloor = floor;
         }
     }
