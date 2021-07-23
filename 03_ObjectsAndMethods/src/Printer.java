5fcd7f21686344e8ea52a11810a896acc623f8e8
mariia_luchnikova <luchniklav@yandex.ru>
2021-07-23 23:30:12 +0800

    correction Printer.java

--- 03_ObjectsAndMethods/src/Printer.java
+++ 03_ObjectsAndMethods/src/Printer.java
@@ -1,36 +1,26 @@
-import javax.swing.text.Document;
-
 public class Printer {
-    private static int count = 0; // начальное значение количества чего-то
-    private int kolDoc; // переменная, в которой хранится кол-во документов, добавленных в очередь
     private String queue = ""; // переменная, в которой хранится список документов на печать
-    private int totalKolPage = 0; // общее количество страниц, добавленных в принтер
+    private int totalKolPending = 0; // общее количество страниц, добавленных в принтер
+    private int totalKolPrinted = 0; // общее количество распечатанных страниц
 
-    public Printer(String queue, int kolDoc) {  // конструктор очереди печати списка документов
+    public Printer(String queue) {  // конструктор очереди печати списка документов
         this.queue += queue;
-        this.kolDoc = kolDoc;
-    }
-
-    public static int getCount() { // метод для получения количества чего-то
-        return count;
-    }
-
-    // метод для определения количества очередей печати ???
-    public static void getQueueCount(int count) {
-        Printer.count = Printer.count + count;
     }
 
     public static void main(String[] args) {
-        Printer printer = new Printer("", 5);
-        //for (int k = 1; k <= printer.kolDoc; k++) {  // для примера отправлено на печать 5 документов
-        printer.append("Текст документа"); // пример отправки документов на печать без цикла по количеству документов
-        printer.append("Текст документа", "Название документа", 2);
-        //}
-        printer.print("Список документов, отправленных на печать: ");
+        Printer printer = new Printer("");
+        printer.append("Текст документа1"); // пример отправки документов на печать без цикла по количеству документов
+        printer.append("Текст документа2", "Название документа2", 2);
+        printer.print("Очередь печати:");
+        printer.append("Текст документа3", "Название документа3", 3);
+        printer.print("Очередь печати:");
+        printer.append("Текст документа4", "Название документа4", 4);
+        printer.print("Очередь печати:");
+        System.out.println("Общее количество распечатанных страниц: " + printer.getPrintedPagesCount());
     }
 
     public void append(String txtDoc) {  // метод, принимающий один параметр
-        append(txtDoc, "Название документа");
+        append(txtDoc, "Название документа1");
     }
 
     public void append(String txtDoc, String nameDoc) {   // метод, принимающий два параметра
@@ -39,28 +29,33 @@ public class Printer {
 
     // метод, принимающий три параметра
     public void append(String txtDoc, String nameDoc, int kolPage) {
-        queue = queue + txtDoc + ", " + nameDoc + ", " + "кол-во страниц: " + kolPage + "\n";
-        totalKolPage = totalKolPage + kolPage;
+        queue = queue + "Документ: " + nameDoc + "\n" + "количество страниц: " + kolPage + "\n";
+        totalKolPending += kolPage;
+
     }
 
     public void clear() { // метод, очищающий очередь печати
         queue = "";
-        totalKolPage = 0;
+        totalKolPending = 0;
     }
 
     // метод для определения кол-ва страниц, которые добавлены в очередь печати
     public int getPendingPagesCount() {
-        return totalKolPage;
+        return totalKolPending;
+    }
+
+    public int getPrintedPagesCount() {
+        return totalKolPrinted;
     }
 
     public void print(String listDoc) { // вывод информации в консоль и очистка очереди печати
-        System.out.println("Количество страниц в очереди: " + totalKolPage);
         System.out.println(listDoc);
         if (queue.isEmpty()) {
             System.out.println("Очередь печати отсутствует");
         } else {
             System.out.print(queue);
-            System.out.println("Количество распечатанных страниц: " + getPendingPagesCount());
+            totalKolPrinted += getPendingPagesCount();
+            System.out.println("Количество страниц, добавленных в очередь: " + getPendingPagesCount());
             clear();
         }
     }
