92edde083accb0a3ac644716f4556eff916cbe48
mariia_luchnikova <luchniklav@yandex.ru>
2021-08-12 00:35:05 +0800

    add Dates

--- 07_NumbersAndDates/Date/src/main/java/Main.java
+++ 07_NumbersAndDates/Date/src/main/java/Main.java
@@ -1,11 +1,14 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int day = 31;
-        int month = 12;
-        int year = 1990;
+        int day = 14;
+        int month = 7;
+        int year = 1980;
 
         System.out.println(collectBirthdays(year, month, day));
 
@@ -16,7 +19,17 @@ public class Main {
         //TODO реализуйте метод для построения строки в следующем виде
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
-        
-        return "";
+        LocalDate today = LocalDate.now();
+        LocalDate birthday = LocalDate.of(year, month, day);
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(" - dd.MM.yyyy - EE", Locale.US);
+
+        int i = 0;
+        String str = "";
+        while (birthday.isBefore(today.plusDays(1))) {
+            str = str + i + dateFormatter.format(birthday) + System.lineSeparator();
+            birthday = birthday.plusYears(1);
+            i++;
+        }
+        return str;
     }
 }
--- 07_NumbersAndDates/Periods/src/main/java/Main.java
+++ 07_NumbersAndDates/Periods/src/main/java/Main.java
@@ -1,13 +1,20 @@
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Main {
+    private static final int day = 23;
+    private static final int month = 5;
+    private static final int year = 1995;
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        LocalDate birthday = LocalDate.of(year, month, day);
+        System.out.println(getPeriodFromBirthday(birthday));
+    }
 
-  }
-
-  private static String getPeriodFromBirthday(LocalDate birthday) {
-    return "";
-  }
+    public static String getPeriodFromBirthday(LocalDate birthday) {
+        LocalDate today = LocalDate.now();
+        Period period = Period.between(birthday, today);
+        return "" + period.getYears() + " years, " + period.getMonths() + " months, " + period.getDays() + " days";
+    }
 
 }
