ea262912537455cf87f957dec4e1a23baf85cddc
mariia_luchnikova <luchniklav@yandex.ru>
2021-10-19 20:56:11 +0800

    add TotalEarningsCalculatorReqExp

--- 08_Strings/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,23 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        System.out.println(calculateSalarySum("Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей"));
+    }
 
-  }
-
-  public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
-  }
-
+    public static int calculateSalarySum(String text) {
+        //TODO: реализуйте метод
+        String regex = "[0-9]{1,}";
+        int sumSalary = 0;
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            int start = matcher.start();
+            int end = matcher.end();
+            sumSalary = sumSalary + Integer.parseInt(text.substring(start, end));
+        }
+        return sumSalary;
+    }
 }
