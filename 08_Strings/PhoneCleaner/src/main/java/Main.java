019e41f0c7253cc414e86bd0fbf2cb117805784c
mariia_luchnikova <luchniklav@yandex.ru>
2021-11-03 14:02:30 +0800

    add PhoneCleaner1

--- 08_Strings/PhoneCleaner/src/main/java/Main.java
+++ 08_Strings/PhoneCleaner/src/main/java/Main.java
@@ -2,16 +2,34 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            //TODO:напишите ваш код тут, результат вывести в консоль.
+            String regexClearing = "[^0-9]";
+            String phoneNumber = input.replaceAll(regexClearing, "");
+            switch (phoneNumber.length()) {
+                case 10:
+                    phoneNumber = "7" + phoneNumber;
+                    System.out.println(phoneNumber);
+                    break;
+                case 11:
+                    if (phoneNumber.charAt(0) == '7' || phoneNumber.charAt(0) == '8') {
+                        phoneNumber = phoneNumber.replaceFirst("8", "7");
+                        System.out.println(phoneNumber);
+                    } else
+                        System.out.println("Неверный формат номера");
+                    break;
+                default:
+                    System.out.println("Неверный формат номера");
+                    break;
+            }
+        }
     }
-  }
 
 }
