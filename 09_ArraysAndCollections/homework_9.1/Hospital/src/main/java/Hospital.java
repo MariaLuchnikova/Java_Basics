dd78d62294c2944d0ec715a913e3c5eeebbebff3
mariia_luchnikova <luchniklav@yandex.ru>
2021-11-06 22:38:07 +0800

    add Hospital

--- 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
+++ 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
@@ -1,10 +1,17 @@
 public class Hospital {
+    public static int lowerBound = 32;
+    public static int upperBound = 40;
+    public static double minTemperatureHealthy = 36.2;
+    public static double maxTemperatureHealthy = 36.9;
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
 
         //TODO: напишите метод генерации массива температур пациентов
-
-        return new float[0];
+        float[] patientsTemperature = new float[patientsCount];
+        for (int i = 0; i < patientsCount; i++) {
+            patientsTemperature[i] = ((float) Math.random() * (upperBound - lowerBound)) + lowerBound;
+        }
+        return patientsTemperature;
     }
 
     public static String getReport(float[] temperatureData) {
@@ -12,11 +19,19 @@ public class Hospital {
         TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
             а также температуры всех пациентов.
         */
+        float sumTemperature = 0;
+        int countHealthy = 0;
+        String strArray = "";
+        for (float array : temperatureData) {
+            strArray += Math.round(array * 10f) / 10f + " ";
+            sumTemperature += array;
+            countHealthy += (array >= (float) minTemperatureHealthy && array <= (float) maxTemperatureHealthy ? 1 : 0);
+        }
 
         String report =
-                "Температуры пациентов: " + 0 +
-                        "\nСредняя температура: " + 0 +
-                        "\nКоличество здоровых: " + 0;
+                "Температуры пациентов: " + strArray.trim() +
+                        "\nСредняя температура: " + Math.round(sumTemperature / temperatureData.length * 100f) / 100f +
+                        "\nКоличество здоровых: " + countHealthy;
 
         return report;
     }
