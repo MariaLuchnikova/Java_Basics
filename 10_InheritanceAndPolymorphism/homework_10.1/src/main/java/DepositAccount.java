ddcebd7dd688615b448f86b5abeb6b95d53af8ca
mariia_luchnikova <luchniklav@yandex.ru>
2021-11-30 20:45:18 +0800

    add BankAccount

--- 10_InheritanceAndPolymorphism/homework_10.1/src/main/java/DepositAccount.java
+++ 10_InheritanceAndPolymorphism/homework_10.1/src/main/java/DepositAccount.java
@@ -1,3 +1,30 @@
-public class DepositAccount extends BankAccount {
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
+public class DepositAccount extends BankAccount {
+    private LocalDate lastIncome;
+
+    @Override
+    public double getAmount() {
+        return super.getAmount();
+    }
+
+    @Override
+    public void setAmount(double amount) {
+        super.setAmount(amount);
+    }
+
+    @Override
+    public void put(double amountToPut) {
+        super.put(amountToPut);
+        lastIncome = LocalDate.now();
+    }
+
+    @Override
+    public void take(double amountToTake) {
+        if (ChronoUnit.MONTHS.between(lastIncome, LocalDate.now()) >= 1)
+            super.take(amountToTake);
+        else
+            System.out.println("Запрет выполнения операции списания. С даты последнего пополнения " + lastIncome + " прошло менее одного месяца.");
+    }
 }
